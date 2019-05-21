54dbd58b10e0922c3be146be1b631e3fb21c48bf
lesliecdubs <leslie@netlify.com>
2019-05-20 22:46:45 -0500

    Removing Flyers section from Comedy page

--- src/pages/comedy.js
+++ src/pages/comedy.js
@@ -1,13 +1,19 @@
 import React, { Component, Fragment } from 'react'
 import { graphql } from 'gatsby'
-import { Layout, TourDateGroup, Flyer, LinkBtn, VideoSection } from '../components'
-import { 
-  normalizeComedyPage, 
-  normalizeTourDate, 
-  getUpcomingShows, 
-  getPastShows, 
-  sortByAscendingDate, 
-  sortByDescendingDate 
+import {
+  Layout,
+  TourDateGroup,
+  Flyer,
+  LinkBtn,
+  VideoSection,
+} from '../components'
+import {
+  normalizeComedyPage,
+  normalizeTourDate,
+  getUpcomingShows,
+  getPastShows,
+  sortByAscendingDate,
+  sortByDescendingDate,
 } from '../helpers'
 import Img from 'gatsby-image'
 
@@ -23,53 +29,25 @@ class ComedyPage extends Component {
   }
 
   render() {
-    const { headline, hero, heroAlt, description, flyers, videos } = this._comedyPage
-    const upcomingShows = sortByAscendingDate(getUpcomingShows(this._comedyShows))
+    const { headline, hero, heroAlt, description, videos } = this._comedyPage
+    const upcomingShows = sortByAscendingDate(
+      getUpcomingShows(this._comedyShows)
+    )
     const pastShows = sortByDescendingDate(getPastShows(this._comedyShows))
 
     return (
       <Layout style="page--scroll" pageName={headline}>
-        <Img 
-          fluid={hero} 
-          alt={heroAlt} 
-          className="is-visible-sm" 
-        />
+        <Img fluid={hero} alt={heroAlt} className="is-visible-sm" />
         <p>{description}</p>
 
         {upcomingShows && upcomingShows.length > 0 && (
-          <TourDateGroup 
-            title="Upcoming Comedy" 
-            shows={upcomingShows} 
-          />
-        )}
-
-        {flyers && (
-          <section>
-            <h2>Flyers</h2>
-            <ul className="flyers">
-              {flyers.map((flyer, i) => {
-                return (
-                  <li key={i} className="flyer">
-                    <Flyer 
-                      title={flyer.title} 
-                      file={flyer.file} 
-                      image={flyer.image} 
-                    />
-                  </li>
-                )
-              })}
-            </ul>
-          </section>
+          <TourDateGroup title="Upcoming Comedy" shows={upcomingShows} />
         )}
 
         {videos && <VideoSection videos={videos} title="Comedy Clips" />}
 
         {pastShows && pastShows.length > 0 && (
-          <TourDateGroup 
-            title="Past Comedy" 
-            shows={pastShows}
-            past={true}
-          />
+          <TourDateGroup title="Past Comedy" shows={pastShows} past={true} />
         )}
       </Layout>
     )
@@ -90,9 +68,6 @@ export const query = graphql`
           }
           description {
             description
-          }
-          showFlyers {
-            ...FlyerFragment
           }
           comedyVideos {
             videoTitle
