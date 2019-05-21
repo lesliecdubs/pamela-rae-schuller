761e96ab00d74dd3f5079c38c1fb3379bf16a76e
lesliecdubs <leslie@netlify.com>
2019-05-20 22:43:52 -0500

    Limiting upcoming shows to max of 5

--- src/helpers/date-helpers.js
+++ src/helpers/date-helpers.js
@@ -2,22 +2,23 @@ import moment from 'moment'
 
 export function getUpcomingShows(showList) {
   const now = moment()
-  return showList.filter(show => moment(show.date).isAfter(now));
+  return showList.filter(show => moment(show.date).isAfter(now))
 }
 
 export function getPastShows(showList) {
   const now = moment()
-  return showList.filter(show => moment(show.date).isBefore(now));
+  return showList.filter(show => moment(show.date).isBefore(now))
 }
 
 export function sortByAscendingDate(array) {
-  return array.sort(function(a, b) {
+  const sortedArray = array.sort(function(a, b) {
     return new Date(a.date) - new Date(b.date)
-  });
+  })
+  return sortedArray.length > 5 ? sortedArray.slice(0, 5) : sortedArray
 }
 
 export function sortByDescendingDate(array) {
   return array.sort(function(a, b) {
     return new Date(b.date) - new Date(a.date)
-  });
+  })
 }
