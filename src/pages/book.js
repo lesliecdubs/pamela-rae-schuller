6bac037fc8b9131a66bb6e40c5b09f0b3c0e24e7
lesliecdubs <leslie@netlify.com>
2019-05-24 16:48:06 -0500

    Moving Book Now form above packages

--- src/pages/book.js
+++ src/pages/book.js
@@ -23,22 +23,155 @@ class BookingPage extends Component {
 
     return (
       <Layout style="page--scroll" pageName={title}>
-        <h2 className="is-visually-hidden">Packages</h2>
-        <ul className="packages">
-          {packages.map((pack, i) => (
-            <li key={i} className="packages__package">
-              <Img
-                fluid={pack.image}
-                alt={pack.imageAlt}
-                className="packages__package-image"
-              />
-              <div>
-                <h3 className="packages__package-title">{pack.name}</h3>
-                <p className="packages__package-info">{pack.description}</p>
-              </div>
-            </li>
-          ))}
-        </ul>
+        <Img
+          fluid={separator}
+          alt={separatorAlt}
+          className="gig-separator is-visible-sm"
+        />
+
+        <form
+          className="form"
+          name="book-now"
+          method="post"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+        >
+          <input type="hidden" name="book-now" value="book-now" />
+
+          <div className="form__group form__group--split">
+            <label className="form__label" htmlFor="name">
+              Full name*
+            </label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="First Last"
+              required
+            />
+          </div>
+
+          <div className="form__group form__group--split">
+            <label className="form__label" htmlFor="email">
+              Email*
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="email@me.com"
+              required
+            />
+          </div>
+
+          <div className="form__group form__group--split">
+            <label className="form__label" htmlFor="phone">
+              Phone
+            </label>
+            <input
+              type="tel"
+              name="phone"
+              id="phone"
+              placeholder="(222) 222-2222"
+            />
+          </div>
+
+          <div className="form__group form__group--split">
+            <label className="form__label" htmlFor="org">
+              Company
+            </label>
+            <input
+              type="text"
+              name="org"
+              id="org"
+              placeholder="Organization"
+            />
+          </div>
+
+          <div className="form__group form__group--split">
+            <label className="form__label" htmlFor="date">
+              Ideal date
+            </label>
+            <input
+              type="text"
+              id="date"
+              name="date"
+              placeholder="MM/DD/YYYY"
+            />
+          </div>
+
+          <div className="form__group form__group--split">
+            <label className="form__label" htmlFor="age">
+              Age group
+            </label>
+            <input type="age" name="age" id="age" placeholder="18+" />
+          </div>
+
+          <div className="form__group form__group--checkboxes">
+            <p className="form__label">Type of show</p>
+            <div className="form__group--checkboxes__all">
+              <label className="checkbox">
+                Keynote
+                <input type="checkbox" name="typeOfShow" value="Keynote" />
+                <span className="checkbox__box" tabIndex="1" />
+              </label>
+              <label className="checkbox">
+                Workshop
+                <input type="checkbox" name="typeOfShow" value="Workshop" />
+                <span className="checkbox__box" tabIndex="1" />
+              </label>
+              <label className="checkbox">
+                Professional development
+                <input
+                  type="checkbox"
+                  name="typeOfShow"
+                  value="Professional development"
+                />
+                <span className="checkbox__box" tabIndex="1" />
+              </label>
+              <label className="checkbox">
+                Motivational
+                <input type="checkbox" name="typeOfShow" value="Motivational" />
+                <span className="checkbox__box" tabIndex="1" />
+              </label>
+              <label className="checkbox">
+                Stand-up
+                <input type="checkbox" name="typeOfShow" value="Stand-up" />
+                <span className="checkbox__box" tabIndex="1" />
+              </label>
+              <label className="checkbox">
+                Other
+                <input type="checkbox" name="typeOfShow" value="Other" />
+                <span className="checkbox__box" tabIndex="1" />
+              </label>
+            </div>
+          </div>
+
+          <div className="form__group">
+            <button type="submit" className="cta">
+              Submit
+            </button>
+          </div>
+        </form>
+
+        <section>
+          <h2 className="is-visually-hidden">Packages</h2>
+          <ul className="packages">
+            {packages.map((pack, i) => (
+              <li key={i} className="packages__package">
+                <Img
+                  fluid={pack.image}
+                  alt={pack.imageAlt}
+                  className="packages__package-image"
+                />
+                <div>
+                  <h3 className="packages__package-title">{pack.name}</h3>
+                  <p className="packages__package-info">{pack.description}</p>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </section>
 
         <section className="gigs">
           <p className="gigs__intro">
@@ -52,132 +185,6 @@ class BookingPage extends Component {
               </li>
             ))}
           </ul>
-        </section>
-
-        <Img
-          fluid={separator}
-          alt={separatorAlt}
-          className="gig-separator is-visible-sm"
-        />
-
-        <section className="form">
-          <h2>Book Now</h2>
-          <form
-            name="book-now"
-            method="post"
-            data-netlify="true"
-            data-netlify-honeypot="bot-field"
-          >
-            <input type="hidden" name="book-now" value="book-now" />
-
-            <div className="form__group form__group--split">
-              <label className="form__label" htmlFor="name">
-                Full name*
-              </label>
-              <input
-                type="text"
-                name="name"
-                id="name"
-                placeholder="First Last"
-                required
-              />
-            </div>
-
-            <div className="form__group form__group--split">
-              <label className="form__label" htmlFor="email">
-                Email*
-              </label>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                placeholder="email@me.com"
-                required
-              />
-            </div>
-
-            <div className="form__group form__group--split">
-              <label className="form__label" htmlFor="phone">
-                Phone
-              </label>
-              <input
-                type="tel"
-                name="phone"
-                id="phone"
-                placeholder="(222) 222-2222"
-              />
-            </div>
-
-            <div className="form__group form__group--split">
-              <label className="form__label" htmlFor="org">
-                Company
-              </label>
-              <input
-                type="text"
-                name="org"
-                id="org"
-                placeholder="Organization"
-              />
-            </div>
-
-            <div className="form__group form__group--split">
-              <label className="form__label" htmlFor="date">
-                Ideal date
-              </label>
-              <input
-                type="text"
-                id="date"
-                name="date"
-                placeholder="MM/DD/YYYY"
-              />
-            </div>
-
-            <div className="form__group form__group--split">
-              <label className="form__label" htmlFor="age">
-                Age group
-              </label>
-              <input type="age" name="age" id="age" placeholder="18+" />
-            </div>
-
-            <div className="form__group form__group--checkboxes">
-              <p className="form__label">Type of show</p>
-              <label className="checkbox">
-                Performance
-                <input type="checkbox" name="typeOfShow" value="Performance" />
-                <span className="checkbox__box" tabIndex="1" />
-              </label>
-              <label className="checkbox">
-                Workshop
-                <input type="checkbox" name="typeOfShow" value="Workshop" />
-                <span className="checkbox__box" tabIndex="1" />
-              </label>
-              <label className="checkbox">
-                Professional Development
-                <input
-                  type="checkbox"
-                  name="typeOfShow"
-                  value="Professional Development"
-                />
-                <span className="checkbox__box" tabIndex="1" />
-              </label>
-              <label className="checkbox">
-                Motivational
-                <input type="checkbox" name="typeOfShow" value="Motivational" />
-                <span className="checkbox__box" tabIndex="1" />
-              </label>
-              <label className="checkbox">
-                Other
-                <input type="checkbox" name="typeOfShow" value="Motivational" />
-                <span className="checkbox__box" tabIndex="1" />
-              </label>
-            </div>
-
-            <div className="form__group">
-              <button type="submit" className="cta">
-                Submit
-              </button>
-            </div>
-          </form>
         </section>
       </Layout>
     )
