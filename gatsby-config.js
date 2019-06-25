ea75473fb350398442fe0b9eb41920d0f674d01a
lesliecdubs <leslie@netlify.com>
2019-06-25 17:11:01 -0500

    Add force trailing slashes

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
+    'gatsby-plugin-force-trailing-slashes',
     // 'gatsby-plugin-remove-trailing-slashes', 
     'gatsby-plugin-sass',
     {
