88e51a16d6158731071ff1e47f2e96c72af634ed
Leslie Cohn-Wein <leslie@cohnwein.com>
2020-08-25 18:02:29 -0500

    Update 404 copy

--- src/pages/404.js
+++ src/pages/404.js
@@ -12,7 +12,7 @@ class NotFoundPage extends Component {
           <Img className="four-oh-four__image" fluid={pamHotMess.childImageSharp.fluid} />
           <section className="four-oh-four__copy">
             <h2>Well, this is awkward...</h2>
-            <p>That page doesn't exist.{" "}
+            <p>That page doesn't exist yet.{" "}
               <a className="link--underline" href="/">
                 <span>Go home!</span>
               </a>
