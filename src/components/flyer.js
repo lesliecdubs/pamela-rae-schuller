476fb52e31dee92a1f9b73cacb4eecc22658f5f3
Leslie Cohn-Wein <leslie@cohnwein.com>
2020-08-25 18:36:42 -0500

    Styling and customizing 404 page (#6)

--- src/components/flyer.js
+++ src/components/flyer.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { LinkBtn } from './'
 import Img from 'gatsby-image'
 
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Layout from '../components/layout'
+import Img from 'gatsby-image'
 
-const NotFoundPage = () => (
-  <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+class NotFoundPage extends Component {
+  render() {
+    const { pamHotMess } = this.props.data
+
+    return (
+      <Layout pageName="Not Found" style="page--scroll">
+        <div className="four-oh-four">
+          <Img className="four-oh-four__image" fluid={pamHotMess.childImageSharp.fluid} />
+          <section className="four-oh-four__copy">
+            <h2>Well, this is awkward...</h2>
+            <p>That page doesn't exist yet.{" "}
+              <a className="link--underline" href="/">
+                <span>Go home!</span>
+              </a>
+            </p>
+          </section>
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export const query = graphql`
+  query FourOhFourPageQuery {
+    pamHotMess: file(relativePath: { eq: "pam-hot-mess.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 1600) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
 
 export default NotFoundPage
